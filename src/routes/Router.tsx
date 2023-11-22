06338484c788f3fd77fd53e3099b56f9f4b65526
Meirasulin <asulinmeir64@gmail.com>
2023-11-22 10:40:23 +0200

    meir commit

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import ROUTES from "./RoutesModel";
 import { SignIn } from "../features/users/login/LoginPage";
 import { SignUp } from "../features/users/signUp/SignUp";
+import InventoryPage from "../features/inventory/inventory/allProductsDisplay/page/InventoryPage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path={ROUTES.login_page} element={<SignIn />} />
+      <Route path={ROUTES.inventory} element={<InventoryPage />} />
       <Route path={ROUTES.sign_up} element={<SignUp />} />
       <Route path={ROUTES.ERROR} element={""} />
     </Routes>
