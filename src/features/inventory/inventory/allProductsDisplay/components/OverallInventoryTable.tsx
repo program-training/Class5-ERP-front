d0819a7b3382db9e07e09d9e6f834c125bcd8d34
Igor Lisbaron <lisbaron.igor@gmail.com>
2023-11-23 11:23:00 +0200

    style and connect overall inventory table component

--- src/features/inventory/inventory/allProductsDisplay/components/OverallInventoryTable.tsx
+++ src/features/inventory/inventory/allProductsDisplay/components/OverallInventoryTable.tsx
@@ -1,30 +1,70 @@
-import Table from "@mui/material/Table";
-import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper";
-import TableHeadModel from "../models/TableHead";
-import { TableBody } from "@mui/material";
-import { StyledTableCell } from "../styles/styleLabelCell";
-import { StyledTableRow } from "../styles/styleLabelRow";
-
-const a = [1, 2, 3];
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import Divider from '@mui/material/Divider';
+import { useAppSelector } from "../../../../../redux/hooks";
+import { getProductsDetails } from "../helpers/helpers";
 
 const OverallInventoryTable = () => {
+  const products = useAppSelector((store) => store.inventory.inventoryProducts);
+  const {sumCategories, sumProducts, sumLowInStock, sumOutOfStock} = getProductsDetails(products.allProducts);
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 700 }}>
-          <TableHeadModel viewImage={false} />
-          <TableBody>
-            <StyledTableRow>
-              {a.map((value, key) => (
-                <StyledTableCell align="center" key={key}>
-                  {value}
-                </StyledTableCell>
-              ))}
-            </StyledTableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Box
+        sx={{
+          flexGrow: 1,
+          width: "100%",
+          borderRadius: 4,
+          bgcolor: '#F0F1F3',
+          paddingTop: "15px",
+          paddingBottom: "15px"
+        }}
+      >
+        <Grid container sx={{height:"100%", padding:"10px"}}>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Box sx={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+              <Stack direction="column"
+                      alignItems="flex-start"
+                      spacing={3}>
+                <Typography variant="h4" sx={{color:"#1570EF"}}>Categories</Typography>
+                <Typography variant="h6" sx={{color:"gray"}}>all the categories: {sumCategories}</Typography>
+              </Stack>
+              <Divider orientation="vertical" sx={{marginLeft:"80px"}}/>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Box sx={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+              <Stack direction="column"
+                      alignItems="flex-start"
+                      spacing={3}>
+                <Typography variant="h4" sx={{color:"#E19133"}}>Total Products</Typography>
+                <Typography variant="h6" sx={{color:"gray"}}>sum of all products: {sumProducts}</Typography>
+              </Stack>
+              <Divider orientation="vertical" sx={{marginLeft:"80px"}}/>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Box sx={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+              <Stack direction="column"
+                      alignItems="flex-start"
+                      spacing={3}>
+                <Typography variant="h4" sx={{color:"#845EBC"}}>Low Stocks</Typography>
+                <Typography variant="h6" sx={{color:"gray"}}>products below 5 in stock: {sumLowInStock}</Typography>
+              </Stack>
+              <Divider orientation="vertical" sx={{marginLeft:"80px"}}/>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Box sx={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+              <Stack direction="column"
+                      alignItems="flex-start"
+                      spacing={3}>
+                <Typography variant="h4" sx={{color:"#F36960"}}>Not In Stocks</Typography>
+                <Typography variant="h6" sx={{color:"gray"}}>products out of stocks: {sumOutOfStock}</Typography>
+              </Stack>
+            </Box>
+          </Grid>
+        </Grid>
+      
+      </Box>
     </>
   );
 };
