7110175ba4aba45648507615c5ad694a794eb424
itzik <itsikzigi@gmail.com>
2023-11-23 11:14:43 +0200

    fixed

--- src/features/users/login/LoginPage.tsx
+++ src/features/users/login/LoginPage.tsx
@@ -1,19 +1,46 @@
 import TextField from "@mui/material/TextField";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { S1, S2 } from "./components/Style";
+import { inputs } from "./components/Validation";
+import { ErrorMessage } from "@hookform/error-message";
+import Typography from "@mui/material/Typography";
+import { Password_validation, Email_validation } from "./components/Validation";
+import { Grid } from "@mui/material";
+import BottomLinks from "./components/BottomLinks";
+import { SubButton } from "./components/SubButton";
+import { TopPage } from "./components/TopPage";
+import { useAppDispatch } from "../../../redux/hooks";
+import { setUser } from "../userSlice";
+import { To, useNavigate } from "react-router-dom";
+import loginReq from "./service/loginReq";
 
-
-
+const LogIn = () => {
+  const dispatch = useAppDispatch();
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>({ mode: "onChange" });
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  } = useForm<inputs>({ mode: "onChange", criteriaMode: "all" });
+  const navigate = useNavigate();
+  const navigateTo = (to: To) => navigate(to);
+
+  const onSubmit = (data: inputs) => {
+    loginReq(data)
+      .then(() => {
+        dispatch(setUser(data.email));
+        navigateTo("/");
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{ border: "0.1px solid green", minWidth: "40vw" }}
+    >
       <Box sx={S1}>
         <TopPage />
         <Box
@@ -22,27 +49,52 @@ import Box from "@mui/material/Box";
           noValidate
           sx={{ mt: 1 }}
         >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Email Address"
-            autoComplete="email"
-            autoFocus
-            {...register("email", { required: true })}
-          />
-          {errors.email && <span>This field is required</span>}
-
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            {...register("password", { required: true })}
-          />
-          {errors.email && <span>This field is required</span>}
+          <Grid>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email"
+              autoComplete="@gmail.com"
+              autoFocus
+              {...register("email", Email_validation)}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="email"
+              render={({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <Typography sx={S2} key={type}>
+                    {message}
+                  </Typography>
+                ))
+              }
+            />
+          </Grid>
+          <Grid>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              {...register("password", Password_validation)}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="password"
+              render={({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <Typography key={type} sx={S2}>
+                    {message}{" "}
+                  </Typography>
+                ))
+              }
+            />
+          </Grid>
 
           <BottomLinks />
           <SubButton />
@@ -50,6 +102,6 @@ import Box from "@mui/material/Box";
       </Box>
     </Container>
   );
-}
+};
 
 export default LogIn;
