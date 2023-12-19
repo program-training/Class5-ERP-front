8e3f2d2c15efe4c8eb4dcad02f29c65714fe000a
moshedoytsh <moshedoytsh@gmail.com>
2023-12-19 13:07:45 +0200

    subscription in client, module changes

--- src/features/inventory/productDisplay/components/ProductDetails.tsx
+++ src/features/inventory/productDisplay/components/ProductDetails.tsx
@@ -6,15 +6,8 @@ import AppBarModel from "../models/AppBar";
 import Details from "../models/Details";
 import { useAppDispatch, useAppSelector } from "../../../../redux/hooks";
 import { setOpenPageProducts } from "../../productsDisplay/utils/inventorySlice";
-import { useQuery } from "@apollo/client";
-import { GET_CHANGE } from "../../../../apollo/queries-temporary-location/get-change-on-product ID";
 import StatistsGraph from "./StatistsGraph";
-import MessagePendingOrError from "../../productsDisplay/components/MessagePendingOrError";
 
-interface ProductStatistics {
-  current_quantity: number;
-  changed_on: string;
-}
 const ProductDetails = () => {
   const dispatch = useAppDispatch();
   const { chosenProduct } = useAppSelector(
@@ -23,15 +16,8 @@ const ProductDetails = () => {
   const { openProductPage } = useAppSelector(
     (store) => store.inventory.inventoryProducts
   );
-  const { data, loading, error } = useQuery(GET_CHANGE, {
-    variables: { getProductStatisticsId: chosenProduct?.id || '11111111' },
-  });
 
   const [openUpdate, setOpenUpdate] = useState(false);
-
-  const dates: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  // const dates: number[] = [];
-  const quantity: number[] = [];
 
   if (!chosenProduct) return;
 
@@ -51,18 +37,7 @@ const ProductDetails = () => {
         <Box sx={{ display: "flex", margin: 1, maxWidth: "100%" }}>
           <Box>
             <Details product={chosenProduct} />
-            {loading && (
-              <MessagePendingOrError message={"load"} title={"load products"} />
-            )}
-            {!data && error && (
-              <MessagePendingOrError message={error.message} title={"error"} />
-            )}
-            {data &&
-              data.getProductStatistics.map((p: ProductStatistics) => {
-                // dates.push(new Date(p.changed_on).getDate());
-                quantity.push(p.current_quantity);
-              })}
-            <StatistsGraph dates={dates} quantity={quantity} />
+            <StatistsGraph productId={chosenProduct.id} />
           </Box>
           <Box
             sx={{
--- src/features/inventory/productDisplay/components/StatistsGraph.tsx
+++ src/features/inventory/productDisplay/components/StatistsGraph.tsx
@@ -1,13 +1,38 @@
+import { useQuery } from "@apollo/client";
 import { LineChart } from "@mui/x-charts/LineChart";
+import { GET_CHANGE } from "../../../../apollo/queries-temporary-location/get-change-on-product ID";
+import MessagePendingOrError from "../../productsDisplay/components/MessagePendingOrError";
+
+interface ProductStatistics {
+  current_quantity: number;
+  changed_on: string;
+}
 
 interface Props {
-  dates: number[];
-  quantity: number[];
+  productId: string | number
 }
-const StatistsGraph = ({ dates, quantity }: Props) => {
-  return (
+const StatistsGraph = ({ productId }: Props) => {
+  const dates: number[] = [];
+  const quantity: number[] = [];
+
+  const { data, loading, error } = useQuery(GET_CHANGE, {
+    variables: { getProductStatisticsId: `${productId}` || '11111111' },
+  });
+  
+  return (<>
+    {loading && (
+      <MessagePendingOrError message={"load"} title={"load products"} />
+    )}
+    {!data && error && (
+      <MessagePendingOrError message={error.message} title={"error"} />
+    )}
+    {data &&
+      data.getProductStatistics.map((p: ProductStatistics) => {
+        dates.push(new Date(p.changed_on).getDate());
+        quantity.push(p.current_quantity);
+      })}
     <LineChart
-      xAxis={[{ data: dates }]} // תאריך
+      // xAxis={[{ data: dates }]} // תאריך
       series={[
         {
           data: quantity, // כמות
@@ -17,6 +42,7 @@ const StatistsGraph = ({ dates, quantity }: Props) => {
       width={500}
       height={300}
     />
+    </>
   );
 };
 export default StatistsGraph;
